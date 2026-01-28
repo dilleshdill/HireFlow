b687d28db829e995b07f63282db4efe64f851690
haritarun <tarunbommana798@gmail.com>
2026-01-28 23:46:54 +0530

    ..

---
+++ frontend/src/Pages/NotFound.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import pageNotFoundImage from '../assets/pageNotFoundImage.svg'
+
+const NotFound = () => {
+  return (
+    <div>
+      <img src={pageNotFoundImage} />
+    </div>
+  )
+}
+
+export default NotFound
