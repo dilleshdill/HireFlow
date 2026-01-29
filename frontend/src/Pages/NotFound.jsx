32c0cbd1b4fe0214213924adf267b81408d0ca9f
haritarun <tarunbommana798@gmail.com>
2026-01-29 15:56:06 +0530

    ..

--- frontend/src/Pages/NotFound.jsx
+++ frontend/src/Pages/NotFound.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import pageNotFoundImage from '../assets/pageNotFoundImage.svg'
+import { ArrowRight } from 'lucide-react'
 
 const NotFound = () => {
   return (
-    <div>
-      <img src={pageNotFoundImage} />
+    <div className=' min-w-screen min-h-screen flex justify-center items-center'>
+    <div className='flex justify-center items-center' >
+      <div className='flex flex-col'>
+            <h1 className='text-2xl font-semibold'>Opps! Page not found</h1>
+            <p>something went wrong.It's look this the link is broken or page is remove</p>
+            <div>
+                <div>
+                    <h1>Home</h1>
+                    <ArrowRight/>
+                </div>
+            </div>
+      </div>
+      <img src={pageNotFoundImage} className='h-110 w-auto'/>
+    </div>
     </div>
   )
 }
