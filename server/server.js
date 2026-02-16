8e7b79df92ee80d5f95e52237b0de147095542cd
dilleshdill <dilleshnakkina222@gmail.com>
2026-02-16 19:10:28 +0530

    final commit

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ const app = express()
 app.use(
     cors({
         origin:[
-            'http://localhost:5173',
+            'http://localhost:5173','https://hire-flow-zeta.vercel.app',
                 process.env.FRONTEND_DOMAIN
         ],
         credentials:true,
